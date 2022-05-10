96e8a7ad93986b724c84caa3e4d4471b5057ba7f
Junior Velo <juniorvelo4@gmail.com>
2022-05-09 23:48:15 -0400

    change QuestionDto

--- src/main/java/cu/edu/mes/sigenu/training/core/dto/question/QuestionDto.java
+++ src/main/java/cu/edu/mes/sigenu/training/core/dto/question/QuestionDto.java
@@ -5,7 +5,8 @@ import lombok.Data;
 @Data
 public class QuestionDto {
 
-	private Integer id;
+    private Integer id;
+	private Integer idGroupQuestion;
 	private String question;
 	private String description;
 }
